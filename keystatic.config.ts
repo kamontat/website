7c20b4cbeb7efa819cd5a16ae024d42710511db8
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-12-01 13:50:20 +0700

    perf(store): set up storage to github

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, fields, collection } from '@keystatic/core';
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'github',
+    repo: {
+      owner: 'kamontat',
+      name: 'kamontat',
+    }
   },
   collections: {
     posts: collection({
