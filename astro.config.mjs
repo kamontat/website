36bfe2bb159f9a2dcb0c36c916e7b2ceba6bcf30
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-12-05 14:05:27 +0700

    perf(ci): build application on vercel instead of on GitHub Action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), markdoc(), keystatic()],
+	integrations: [react(), markdoc(), mdx(), keystatic()],
 	output: "hybrid",
 	adapter: vercel({
 		speedInsights: {
@@ -15,12 +16,3 @@ export default defineConfig({
 		},
 	}),
 });
-
-// NOTE: https://github.com/withastro/astro/issues/7573#issuecomment-1817846646
-console.log(
-	process.env.VERCEL_ANALYTICS_ID,
-	process.env.PUBLIC_VERCEL_ANALYTICS_ID,
-);
-if (!process.env.VERCEL_ANALYTICS_ID) {
-	process.env.VERCEL_ANALYTICS_ID = process.env.PUBLIC_VERCEL_ANALYTICS_ID;
-}
