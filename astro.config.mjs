ff0d140c9039a740f7bc9dcfee25f631b4edf1e3
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-12-05 14:23:23 +0700

    perf(vercel): enabled webanalytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import vercel from "@astrojs/vercel/serverless";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), markdoc(), mdx(), keystatic()],
+	site: "https://kc.in.th",
+	integrations: [react(), markdoc(), mdx(), keystatic(), sitemap()],
 	output: "hybrid",
 	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
 		speedInsights: {
 			enabled: true,
 		},
