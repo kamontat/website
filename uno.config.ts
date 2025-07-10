0d7e3ca30749f9b79c7a25aa3f0e0a34de78dda6
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-07-11 00:53:39 +0700

    feat: update unocss config to be usable
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- uno.config.ts
+++ uno.config.ts
@@ -1,3 +1,83 @@
-import { defineConfig } from 'unocss'
+import {
+	defineConfig,
+	presetWind4,
+	presetWebFonts,
+	presetTypography,
+	presetIcons,
+	presetAttributify,
+	transformerDirectives,
+} from "unocss";
 
-export default defineConfig({})
+export default defineConfig({
+	presets: [
+		presetWind4({
+			attributifyPseudo: true,
+			dark: "media",
+		}),
+		presetWebFonts({
+			provider: "fontsource",
+			fonts: {
+				sans: [
+					{
+						// https://fontsource.org/fonts/mitr
+						name: "Mitr",
+						weights: [200, 300, 400, 500],
+						subsets: ["latin", "thai"],
+					},
+				],
+				serif: [
+					{
+						// https://fontsource.org/fonts/mitr
+						name: "Mitr",
+						weights: [200, 300, 400, 500],
+						subsets: ["latin", "thai"],
+					},
+				],
+				mono: [
+					{
+						// https://fontsource.org/fonts/jetbrains-mono
+						name: "JetBrains Mono",
+						subsets: ["latin"],
+						variable: {
+							ital: { default: "0", min: "0", max: "1", step: "1" },
+							wght: { default: "400", min: "100", max: "800", step: "100" },
+						},
+					},
+				],
+			},
+		}),
+		presetTypography({
+			important: true,
+		}),
+		presetIcons({
+			cdn: "https://esm.sh/",
+			prefix: "i-",
+			collections: {
+				"skill-icons": () =>
+					import("@iconify-json/skill-icons/icons.json").then((i) => i.default),
+				"line-md": () =>
+					import("@iconify-json/line-md/icons.json").then((i) => i.default),
+			},
+		}),
+		presetAttributify({
+			prefix: "un-",
+			prefixedOnly: true,
+		}),
+	],
+	transformers: [transformerDirectives()],
+	extendTheme: (theme) => {
+		return {
+			...theme,
+			fontWeight: {
+				thin: "100",
+				extralight: "200",
+				light: "300",
+				normal: "400",
+				medium: "500",
+				semibold: "600",
+				bold: "700",
+				extrabold: "800",
+			},
+		};
+	},
+});
