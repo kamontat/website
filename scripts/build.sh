d1a897ae58ec53cf17497d9acd7a865439e110cc
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-07-14 18:16:01 +0700

    fix(config): custom config should not have higher priority than astro default routes
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ cd "$(dirname "$0")/.."
 
 ## Support deep-merge object and arrays, remove "$comment" field
 # shellcheck disable=SC2016
-QUERY='reduce inputs as $in (.; . + $in + {"routes": (($in.routes | map(del(.["$comment"]))) + .routes)})'
+QUERY='reduce inputs as $in (.; . + $in + {"routes": (.routes + ($in.routes | map(del(.["$comment"]))))})'
 ROOT_CONF="$PWD/vercel.config.json"
 OUTPUT_CONF="$PWD/.vercel/output/config.json"
 TMP_CONF="$PWD/.vercel/output/config.json.tmp"
