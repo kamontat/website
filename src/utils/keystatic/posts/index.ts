b2fd2073bf8b2a9fb5ebe77b4e9cb82d5a4f1a5e
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-07-14 16:25:16 +0700

    fix(page): blog page should omit /blog when visit from blog subdomain
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- src/utils/keystatic/posts/index.ts
+++ src/utils/keystatic/posts/index.ts
@@ -1,6 +1,8 @@
 import { getRelativeLocaleUrl } from "astro:i18n";
 
-export const getPostUrl = (slug: string) => {
+export const getPostUrl = (slug: string, baseUrl: URL) => {
 	const [locale, name] = slug.split("/");
-	return getRelativeLocaleUrl(locale, name, { prependWith: "blog" });
+	return getRelativeLocaleUrl(locale, name, {
+		prependWith: baseUrl.pathname.replaceAll(locale, ""),
+	});
 };
