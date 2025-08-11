c2d8d113b84913e4d154f2dd267da914f3ed192d
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-08-11 11:41:09 +0700

    feat(content): add new link singleton
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

--- src/core/schemas/links.ts
+++ src/core/schemas/links.ts
@@ -2,16 +2,31 @@ import { z } from "astro:schema";
 import { fields } from "@keystatic/core";
 
 import { SingletonDataSchema } from "./models";
+import { ksLocalised, zodLocalised } from "./i18n";
 
 export default new SingletonDataSchema(
 	"links",
 	"Links",
 	z.object({
-		facebook: z.string().url().optional(),
-		x: z.string().url().optional(),
+		link: z
+			.object({
+				name: zodLocalised(z.string()),
+				url: zodLocalised(z.string()),
+			})
+			.array(),
 	}),
 	{
-		facebook: fields.url({ label: "Facebook" }),
-		x: fields.url({ label: "X (Twitter)" }),
+		link: fields.array(
+			fields.object({
+				name: ksLocalised(fields.text, {
+					label: "Name",
+					validation: { isRequired: true },
+				}),
+				url: ksLocalised(fields.url, {
+					label: "URL",
+					validation: { isRequired: true },
+				}),
+			}),
+		),
 	},
 );
